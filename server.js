c38b12d3acd3b1057d4a9a021cb5620078cfd672
Shawon Ashraf <shawon13@live.com>
2018-06-08 00:31:48 +0600

    view engine set

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express"
+import hbs from "hbs"
+
 import { gatherFiles } from "./utils/file-listing"
 
 // init
 let app = express()
 const port = 9000
+
+// register middleware
+app.set("view engine", "hbs")
 
 app.get("/", (req, res) => {
     let path = "files"
