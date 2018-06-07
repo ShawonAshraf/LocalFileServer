91b3e3a3106d451774ee954169f95db3ca8a7678
Shawon Ashraf <shawon13@live.com>
2018-06-08 01:34:50 +0600

    hbs helper

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express"
 import hbs from "hbs"
 import morgan from "morgan"
 
-import { gatherFiles } from "./utils/file-listing"
+import { gatherFiles, hbsTableListing } from "./utils/file-listing"
 
 // init
 let app = express()
@@ -12,11 +12,26 @@ const port = 9000
 app.set("view engine", hbs)
 app.use(morgan("combined"))
 
+// register hbs helper
+hbs.registerHelper("list", hbsTableListing)
+hbs.registerPartials(__dirname + "/views/partials")
+
+// static dir for files
+app.use("/files", express.static(__dirname + "/files"))
+
 app.get("/", (req, res) => {
     let path = "files"
     gatherFiles(path)
         .then(files => {
-            res.send(files)
+            res.render("index.hbs", {
+                files: files,
+                title: "rockash93's Local File Server"
+            })
+        }).catch(err => {
+            res.render("bad.hbs", {
+                err: err,
+                title: "rockash93's Local File Server"
+            })
         })
 })
 
