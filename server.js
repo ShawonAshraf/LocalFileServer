849e1c88358f8d2ae0d054fe690c1ad1773343be
Shawon Ashraf <shawon13@live.com>
2019-08-28 17:16:37 +0600

    async partial

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import hbs from 'hbs';
+import hbs from 'express-hbs';
 import morgan from 'morgan';
 
 import {
@@ -12,12 +12,19 @@ const app = express();
 const port = 3000;
 
 // register middleware
+app.engine('hbs', hbs.express4({
+  partialsDir: `${__dirname}/views/partials`,
+}));
 app.set('view engine', hbs);
+app.set('views', `${__dirname}/views`);
+
 app.use(morgan('combined'));
 
 // register hbs helper
-hbs.registerHelper('list', hbsTableListing);
-hbs.registerPartials(`${__dirname}/views/partials`);
+hbs.registerAsyncHelper('list', (files, cb) => {
+  const content = hbsTableListing(files);
+  cb(new hbs.SafeString(content));
+});
 
 // static dir for files
 app.use('/files', express.static(`${__dirname}/files`));
@@ -26,20 +33,21 @@ app.use('/files', express.static(`${__dirname}/files`));
 app.use(express.static(`${__dirname}/public`));
 
 // replace anonymous with your preferred name
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const path = 'files';
-  gatherFiles(path)
-    .then((files) => {
-      res.render('index.hbs', {
-        files,
-        title: 'Local File Server',
-      });
-    }).catch((err) => {
-      res.render('bad.hbs', {
-        err,
-        title: 'Local File Server',
-      });
+
+  try {
+    const files = await gatherFiles(path, []);
+    res.render('index.hbs', {
+      files,
+      title: 'Local File Server',
     });
+  } catch (err) {
+    res.render('bad.hbs', {
+      err,
+      title: 'Local File Server',
+    });
+  }
 });
 
 app.get('/status', (req, res) => {
