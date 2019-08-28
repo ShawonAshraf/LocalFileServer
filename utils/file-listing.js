e0c05cc57cd3ad5bf3037691816e8842590a4dfc
Shawon Ashraf <shawon13@live.com>
2019-08-28 16:23:14 +0600

    gatherFiles -> async

--- utils/file-listing.js
+++ utils/file-listing.js
@@ -1,31 +1,22 @@
 import fs from 'fs';
 
-export const gatherFiles = path => new Promise((resolve, reject) => {
-  fs.readdir(path, (err, files) => {
-    const fileList = [];
-    if (err) {
-      reject(err);
-    }
-    // add complete path
-    for (let i = 0; i < files.length; i++) {
-      const fileObj = {
-        fileName: `${path}/${files[i]}`,
-      };
-      fileList.push(fileObj);
-    }
-
-    // resolve
-    resolve(fileList);
-  });
-});
+export const gatherFiles = async (path) => {
+  try {
+    const fileList = await fs.readdir(path);
+    return fileList;
+  } catch (e) {
+    return [];
+  }
+};
 
 // lists file urls in hbs view table
-export const hbsTableListing = (files) => {
+export const hbsTableListing = async (files) => {
   let out = '';
   if (files.length === 0) {
     out = 'Server has no files at the moment!';
     return out;
   }
+
   files.forEach((file) => {
     const fileInfo = file.fileName.split('/');
 
